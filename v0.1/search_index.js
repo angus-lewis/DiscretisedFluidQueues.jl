aeac4fa59b7d0f799ea10ab7405d7b6969537516
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-09-27 03:22:29 +0000

    build based on ecf8196

---
+++ v0.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#-1","page":"","title":"","text":"","category":"section"},{"location":"#","page":"","title":"","text":"Documentation goes here.","category":"page"}]
+}
